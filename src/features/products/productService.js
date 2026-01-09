2df1cfca6242cc92960deef61472a682ca83c2a5
ojibona1 <ojibona1@gmail.com>
2026-01-09 09:16:53 +0100

    m

--- src/features/products/productService.js
+++ src/features/products/productService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import uri from "../config";
 
 const getAllProducts =  async(data) => {
-    const response = await axios.post(`${uri}p/api/products`, data);
+    const response = await axios.post(`${uri}/api/products`, data);
     return response.data;
 };
 
